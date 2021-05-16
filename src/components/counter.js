accd90a03d3278426aaff65b0039128db7d5a92e
Raghunathji <partabrajp@gmail.com>
2021-05-16 16:28:21 +0500

    uploading project

---
+++ src/components/counter.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const counter = () => {
+  return <div>hello counter </div>;
+};
+
+export default counter;
---
+++ src/stories/Counter.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import counter from '../components/counter';
+
+export default {
+  title: 'Counter',
+  component: Counter,
+};
+
+export function CounterDefault() {
+  return <counter />;
+}
